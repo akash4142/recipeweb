3e2ed7bf1ae8e66bdd93f64856db34cf86f6654d
akash yadav <64635004+akash4142@users.noreply.github.com>
2024-01-16 22:00:55 -0500

    initial commit

--- server.js
+++ server.js
@@ -1,47 +1,51 @@
 const express = require('express');
 const app = express();
-const {sequelize, Recipe} = require('./models/Recipe');
+const { sequelize, Recipe } = require('./models/Recipe');
 const PORT = process.env.PORT || 8080;
+const path = require('path');
+const hbs = require('hbs'); // Add this line
 
 app.use(express.json());
 app.use(express.static('public'));
-app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'public', 'views'));
 
-app.get('/', async(req, res) => {
-    res.render('home',{title: 'Recipe Website'})
+// Set the view engine to "hbs"
+app.set('view engine', 'hbs');
+hbs.registerPartials(path.join(__dirname, 'public', 'views', 'partials')); // if you have partials
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/new-recipe', (req, res) => {
+  res.render('newrecipe', { title: 'New Recipe' });
 });
 
-app.get('/new-recipe',(req,res)=>{
-    res.render('newrecipe',{title:'New Recipe'});
-})
+app.post('/new-recipe', async (req, res) => {
+  console.log(req.body);
+  const { name, ingredients, instructions } = req.body;
+  try {
+    const newRecipe = await Recipe.create({
+      name,
+      ingredients,
+      instructions,
+    });
+    res.redirect('/');
+  } catch (error) {
+    console.error('Error creating new recipe:', error);
+    res.render('newrecipe', { title: 'New Recipe', error: 'Error creating new recipe' });
+  }
+});
 
-app.post('/new-recipe',async (req,res)=>{
-    const{name,ingredients,instructions} = res.body;
-    try{
-        const newRecipe = await Recipe.create({
-            name,
-            ingredients,
-            instructions
-        })
-        res.redirect('/')
-    }catch (error) {
-        console.error('Error creating new recipe:', error);
-        res.render('new_recipe', { title: 'New Recipe', error: 'Error creating new recipe' });
-    }
-})
-
-app.get('/',async(req,res)=>{
-    try{
-        const recipes = await Recipe.findAll();
-        res.render('home',{title:'Recipe Website',recipes});
-    }catch(error){
-        console.error('Error retrieving recipes: ',error);
-        res.render('home',{title:'Recipe Website',error: 'Error retrieving recipes'})
-    }
-})
-
-
+app.get('/', async (req, res) => {
+  try {
+    const recipes = await Recipe.findAll();
+    res.render('home', { title: 'Recipe Website', recipes });
+  } catch (error) {
+    console.error('Error retrieving recipes: ', error);
+    res.render('home', { title: 'Recipe Website', error: 'Error retrieving recipes' });
+  }
+});
 
 app.listen(PORT, () => {
-    console.log('Server is listening at ' + PORT);
+  console.log('Server is listening at ' + PORT);
 });
